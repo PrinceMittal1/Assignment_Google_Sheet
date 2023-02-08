c61f544dc8d85eecd5ffcd78e4e613e5ce71839e
Prince Mittal <111220289+PrinceMittal1@users.noreply.github.com>
2023-02-08 12:37:06 +0530

    Delete AwesomeProject directory

--- AwesomeProject/App.jsx
+++
@@ -1,6 +0,0 @@
-import {Text, View} from "react-native"
-
-function App(){
-  return <View style={{backgroundColor:"white", flex:1}}><Text style={{color:"black"}}>helo</Text></View>
-}
-export default App;
