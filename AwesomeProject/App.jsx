8d9822043303b659f850fd64901bcf2e5e6f10c4
prince <princemittal345@gmai.com>
2023-02-08 11:41:28 +0530

    first submmison

---
+++ AwesomeProject/App.jsx
@@ -0,0 +1,6 @@
+import {Text, View} from "react-native"
+
+function App(){
+  return <View style={{backgroundColor:"white", flex:1}}><Text style={{color:"black"}}>helo</Text></View>
+}
+export default App;
